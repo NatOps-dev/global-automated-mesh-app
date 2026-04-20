3215640994801756c5bcec294b3d3df49f16dc98
Natcodez <nathisreal2020@gmail.com>
2026-04-20 16:32:27 +0100

    fix: hardcode public key and fix file path dependencies

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,7 +1,7 @@
 #1. SSH Pair (This uploads your public key to the AWS region)
 resource "aws_key_pair" "mesh_deployer1-key" {
   key_name = "mesh_deployer1-key"
-  public_key = file("./deployer1-key.pub")
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDL5jXzEg8ZLsYko6MzkEUwj8XU5BwCt/NW9I3GmXz2nY+lyWGcy9gAzXFQS/Ap1TaG/VfA10fSRjGYhi1dHwSsNpMVBQHwpd2J2IKih0hh4Woe2uqZKfwTgFbViZkR7i/pxYi4dq8RZ2tDlFz2tM+VwGzx+/a6+mLcf7SOZ6P2SfNZ1GJAT7+zOrSOIoV+vAnHjS6QAhTJcaTNh8ahHUKCFpsomXxKuX/BEnWhVtRaXu04WpTQu1KXaL+oOqoEut+MrWqCtowqZvQMrH0v7pbgD5ERogMLn1M8jHaUJHTJa5Vb8D9RKIKtdLxuKNL8VBxuP0/tyV71u9Ees2o0uPAlQudyKHjQjKRgcn17HHRByICvtF1IH1YhiLuQDGyl3NA/soQLdbKZcMIdo03olbRc6B8omN+fVz5rkSdGbM2tvkRqNUu+j368YHXrDDAeCawfNrL01hAUnzAQz6Y/hbuLIynYv/G95qMI4By+sSm+6ldT2jWZee8Yvft02omk6Wc= nathub@Nathaniels-MacBook-Pro.local"
  
 }
 
