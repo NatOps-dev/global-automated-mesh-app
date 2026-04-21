29160d9f130582bf3ddb87188e318ac689f1cab5
Natcodez <nathisreal2020@gmail.com>
2026-04-21 23:33:50 +0100

    fix: cleanup vpc graveyard and update keypair naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,24 @@
-#terraform/maiin.tf 
 resource "aws_instance" "mesh_worker" {
-  # Ubuntu 24.04 LTS AMI for us-west-2
-  ami = "ami-05d38da78ce859165"
+  ami           = "ami-05d38da78ce859165"
   instance_type = "t2.micro"
 
   associate_public_ip_address = true
 
-  # Linking to the networking we built in vpc.tf and security.tf
-  subnet_id = aws_subnet.mesh_public_subnet.id
+  # Linking to the networking built in vpc.tf and security.tf
+  subnet_id       = aws_subnet.mesh_public_subnet.id
   security_groups = [aws_security_group.mesh_sg.id]
-  key_name = aws_key_pair.deployer2-final-key.key_name
+  
+  # This reference now matches the resource name in security.tf
+  key_name        = aws_key_pair.deployer2-final-key.key_name
 
   tags = {
-    Name = " mesh-worker-node"
-    Project = "Global-Automated-Mesh"
+    Name      = "mesh-worker-node"
+    Project   = "Global-Automated-Mesh"
     ManagedBy = "Terraform"
   }
-
 }
 
-# This output is vital for the next step (Ansible)
 output "instance_public_ip" {
   description = "The public IP of the mesh worker node"
-  value = aws_instance.mesh_worker.public_ip
+  value       = aws_instance.mesh_worker.public_ip
 }
