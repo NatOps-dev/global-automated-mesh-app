f7d6b390158c909ed1f428640d59b15c1327f28e
Natcodez <nathisreal2020@gmail.com>
2026-04-22 12:51:13 +0100

    Today's update for the deployment

--- terraform/security.tf
+++ terraform/security.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "mesh_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["34.213.162.113/32"]
+    cidr_blocks = ["34.213.162.113/32"] # Restrict to your IP for security. Replace with your actual IP or Ansible control node's IP.
     # cidr_blocks = ["0.0.0.0/0"] # For testing, allow from anywhere. In production, restrict to your IP or Ansible control node.
   }
 
