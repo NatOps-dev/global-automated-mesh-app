83bc5d211f6ad84bb9cb2c0baac28f8cacaa64cf
Natcodez <nathisreal2020@gmail.com>
2026-04-29 14:01:42 +0100

    FT1.0.: implement python api and postgres database architecture

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,19 @@
 resource "aws_instance" "mesh_worker" {
+  terraform {
+  backend "s3" {
+    bucket = "t2k-terraform-state-bucket"
+    key    = "production/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+
+resource "aws_instance" "mesh_worker" {
   ami           = "ami-05d38da78ce859165"
   instance_type = "t2.micro"
 
   associate_public_ip_address = true
 
-  # FIX: Change 'security_groups' to 'vpc_security_group_ids' 
-  # This prevents Terraform from trying to recreate the instance unnecessarily
   subnet_id                   = aws_subnet.mesh_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.mesh_sg.id]
   
@@ -22,3 +30,5 @@ resource "aws_instance" "mesh_worker" {
 output "instance_public_ip" {
   value = aws_instance.mesh_worker.public_ip
 }
+
+# t2k-terraform-state-bucket
